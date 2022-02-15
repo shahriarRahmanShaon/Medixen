1763f9df3fc598fe249998ef99c5f0b90293f2ea
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-15 21:18:05 +0600

    Initial Commit

---
+++ Medixen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Medixen
+//
+//  Created by sergio shaon on 15/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Medixen/MedixenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedixenApp.swift
+//  Medixen
+//
+//  Created by sergio shaon on 15/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MedixenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
